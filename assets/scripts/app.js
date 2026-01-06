3bcb47c7103834ec0f51a875231b1996ab0fec1d
MALIKSULEMANSAJJAD <malikahanjdz@gmail.com>
2026-01-07 01:31:59 +0500

    third for today

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,4 @@
 console.log("Hellol posse");
 console.log("aight, let's do dis shish");
+console.log("pta ni bhai kab akal ani hai");
 //aronno what imma do with dis stuff, but i got to, 'cause i need "Dolla DOlla"
