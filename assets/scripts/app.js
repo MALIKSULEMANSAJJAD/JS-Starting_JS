5c1a4da18190541e5d00f2834fa63086f295d90f
MALIKSULEMANSAJJAD <malikahanjdz@gmail.com>
2026-01-08 02:24:56 +0500

    3rd

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,5 +2,5 @@ console.log("Hellol posse");
 console.log("aight, let's do dis shish");
 console.log("pta ni bhai kab akal ani hai");
 console.log("Allah se dua hai yaar, akal aa jaye");
-console.log()
+console.log("Kal se papers hain, but jaisay hi papers kahatam hon ge, ma kaam karu ga");
 //aronno what imma do with dis stuff, but i got to, 'cause i need "Dolla DOlla"
