08ab5c815289f807bc09cb0b676bf55b6d4e84fd
MALIKSULEMANSAJJAD <malikahanjdz@gmail.com>
2026-01-09 01:35:42 +0500

    4th

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,5 +2,4 @@ console.log("Hellol posse");
 console.log("aight, let's do dis shish");
 console.log("pta ni bhai kab akal ani hai");
 console.log("Allah se dua hai yaar, akal aa jaye");
-console
 //aronno what imma do with dis stuff, but i got to, 'cause i need "Dolla DOlla"
