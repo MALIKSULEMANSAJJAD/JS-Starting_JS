392f42a6abf1262508959518cd5b67c517249d80
MALIKSULEMANSAJJAD <malikahanjdz@gmail.com>
2026-01-14 03:50:14 +0500

    2nd

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,6 @@
 console.log("Hellol posse");
 console.log("aight, let's do dis shish");
 console.log("pta ni bhai kab akal ani hai");
-console.log("bs kal");
+console.log("bs kal 2nd last paper hai");
 console.log("");
 //aronno what imma do with dis stuff, but i got to, 'cause i need "Dolla DOlla"
