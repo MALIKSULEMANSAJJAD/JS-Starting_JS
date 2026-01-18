b750dcb08aceb2fe149c4a231a55333550af26b9
MALIKSULEMANSAJJAD <malikahanjdz@gmail.com>
2026-01-19 01:15:07 +0500

    4th

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,5 +3,5 @@ console.log("aight, let's do dis shish");
 console.log("pta ni bhai kab akal ani hai");
 console.log("bs kal 2nd last paper hai, phir full nizam laga dena");
 console.log("paper muk gye");
-console.log("");
+console.log("kal se kaam shuru, pakki gal a hun");
 //aronno what imma do with dis stuff, but i got to, 'cause i need "Dolla DOlla"
