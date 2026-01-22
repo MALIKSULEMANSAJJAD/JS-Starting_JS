765dce8c6e7a5a98aa5e6050658496841e448517
MALIKSULEMANSAJJAD <malikahanjdz@gmail.com>
2026-01-23 01:24:43 +0500

    1st

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,4 +2,4 @@ console.log("Hellol posse");
 console.log("aight, let's do dis shish");
 console.log("");
 console.log("");
-//aronno what imma do with dis stuff, but i got to, 'cause i need "Dolla DOlla"
+//aronno what imma do with dis stuff, but i got to, 'cause i
