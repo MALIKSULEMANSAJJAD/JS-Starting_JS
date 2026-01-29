c1991eecd28f671b1756448da409edaa001f777f
MALIKSULEMANSAJJAD <malikahanjdz@gmail.com>
2026-01-30 01:20:06 +0500

    1st

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,4 @@
 console.log("Hellol posse");
 console.log("aight, let's do dis shish");
-console.log("let's start, from now on, imma be serious wit dis shish");
+console.log("let's start, from now on, imma be seri");
 console.log("");
